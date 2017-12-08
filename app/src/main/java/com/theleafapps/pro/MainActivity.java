77ccd39eb3311d42e9d8639e21fbe66e2d87142c
The Aviator <abhi@xyz>
2017-12-09 03:33:00 +0530

    code update for buttons to respond and change the score accordingly

--- app/src/main/java/com/theleafapps/pro/MainActivity.java
+++ app/src/main/java/com/theleafapps/pro/MainActivity.java
@@ -2,12 +2,40 @@ package com.theleafapps.pro;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView scoreView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        displayForTeamA(0);
+    }
+
+    /**
+     * Displays the given score for Team A.
+     */
+    public void displayForTeamA(int score) {
+        scoreView = (TextView) findViewById(R.id.team_a_score);
+        scoreView.setText(String.valueOf(score));
+    }
+
+    public void display3(View view) {
+        scoreView = (TextView) findViewById(R.id.team_a_score);
+        scoreView.setText(String.valueOf(3));
+    }
+
+    public void display2(View view) {
+        scoreView = (TextView) findViewById(R.id.team_a_score);
+        scoreView.setText(String.valueOf(2));
+    }
+
+    public void display1ft(View view) {
+        scoreView = (TextView) findViewById(R.id.team_a_score);
+        scoreView.setText(String.valueOf(1));
     }
 }
